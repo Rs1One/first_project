9ae527b159927b7c3e022dcc9bf7d61b89cb1b06
Alexander Francuzov <afrantsuzov@sugarcrm.com>
2013-09-13 07:14:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a20bc156b8d2d7bf2848b7eaa26cee52136fe9aedffe69526f3169b5aa51923f818a1e77d33718979cebf50b2ddc50c6b3b245e1e0cae857a9feac0bbda3567b'
